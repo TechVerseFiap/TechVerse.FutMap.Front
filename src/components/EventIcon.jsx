caa7872216e488d7dcfddc99bc1394638e93438c
LucasBonato <lucas.perez.bonato@gmail.com>
2025-09-10 21:57:20 -0300

    [FEAT] Remove some images

--- src/components/EventIcon.jsx
+++
@@ -1,5 +0,0 @@
-import eventIcon from "/Event-Icon.svg";
-
-export default function EventIcon({ className, style }) {
-  return <img style={style} className={className} src={eventIcon} alt="" />;
-}
