6cbc30c4372c17d220942f5fcf025b7dd734b189
oliveiralimadiogo <limaa.diogi@gmail.com>
2025-09-10 22:58:05 -0300

    changing primary color

--- src/components/Chip.jsx
+++ src/components/Chip.jsx
@@ -3,7 +3,7 @@ export default function Chip({ label, icon, isActive, onClick }) {
     <button
       onClick={onClick}
       className={`flex justify-between items-center gap-2 px-4 py-1 rounded-full shadow-md transition
-        ${isActive ? "bg-green-500 text-white" : "bg-gray-100 text-black"}
+        ${isActive ? "bg-(--primary-color) text-white" : "bg-gray-100 text-black"}
       `}
     >
       {icon}
