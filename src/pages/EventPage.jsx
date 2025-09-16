4d3b4c4e936f1799c5cfdd5809fe9e4eb3c64b2d
lucassaquino <lucaspo.aquino@gmail.com>
2025-09-16 20:26:26 -0300

    fix(events-page): fixed events page responsivity

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -10,7 +10,29 @@ const events = [
         distance: "2.3Km",
         time: "10:00h",
         people: "23",
-        onJoin: () => {},
+        onJoin: () => { },
+        type: "tournament"
+    },
+    {
+        image: "https://assets.propmark.com.br/uploads/2025/03/472984321_18153546538343800_8892969051646898317_n.jpg",
+        title: "Torneio jovem",
+        description: "Academia de esportes",
+        date: "Amanhã",
+        distance: "2.3Km",
+        time: "10:00h",
+        people: "23",
+        onJoin: () => { },
+        type: "sieve"
+    },
+    {
+        image: "https://conteudo.imguol.com.br/c/esporte/d3/2025/09/07/cruzeiro-e-corinthians-ficaram-no-empate-na-primeira-final-do-brasileirao-feminino-em-belo-horizonte-1757260579540_v2_900x506.jpg",
+        title: "Torneio jovem",
+        description: "Academia de esportes",
+        date: "Amanhã",
+        distance: "2.3Km",
+        time: "10:00h",
+        people: "23",
+        onJoin: () => { },
         type: "tournament"
     },
     {
@@ -51,20 +73,22 @@ export default function EventPage() {
     return (
         <div className="bg-(--bg-white-color) min-h-screen flex flex-col pt-20 px-5">
             <p className="font-semibold">Eventos</p>
-            {events.map((event, index) => (
-                <CardEvent
-                    key={index}
-                    image={ event.image }
-                    title={ event.title }
-                    description={event.description }
-                    date={event.date }
-                    distance={event.distance }
-                    time={ event.time }
-                    people={ event.people }
-                    onJoin={ event.onJoin }
-                    type={ event.type }
-                />
-            ))}
+            <div className="flex flex-wrap justify-center gap-6">
+                {events.map((event, index) => (
+                    <CardEvent
+                        key={index}
+                        image={event.image}
+                        title={event.title}
+                        description={event.description}
+                        date={event.date}
+                        distance={event.distance}
+                        time={event.time}
+                        people={event.people}
+                        onJoin={event.onJoin}
+                        type={event.type}
+                    />
+                ))}
+            </div>
             <p className="font-semibold">Noticias do futebol feminino</p>
             {news.map((item, index) => (
                 <NewsCard
